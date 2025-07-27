53c7fc04eebe36ce409f0623889b828bc7321fea
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-27 23:54:57 +0600

    creating the routing system

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+export default function _Layout() {
+    return (
+        <View>
+            <Text>Auth Layout</Text>
+        </View>
+    )
+}
