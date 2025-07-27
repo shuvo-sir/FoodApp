2784fabe546d34c05787940a20d0f1af60bb22e5
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-27 22:38:36 +0600

    new

--- app/index.tsx
+++ app/index.tsx
@@ -8,19 +8,6 @@ import cn from "clsx";
 export default function Index() {
   return (
       <SafeAreaView className="flex-1 bg-white">
-          <View className={"flex-between flex-row w-full my-5 px-5"}>
-              <View className={"flex-start"}>
-                  <Text className={"small-bold text-primary"}>DELIVER TO</Text>
-                  <TouchableOpacity className={"flex-center flex-row gap-x-1 mt-0.5"}>
-                      <Text className={"paragraph-bold text-dark-100"}>Croatia</Text>
-                      <Image
-                          source={images.arrowDown}
-                          className={"size-3"}
-                          resizeMode={"contain"}
-                      />
-                  </TouchableOpacity>
-              </View>
-          </View>
         <FlatList
             data={offers}
             renderItem={({item, index}) => {
@@ -59,6 +46,23 @@ export default function Index() {
               )
             }}
             contentContainerClassName= "pb-28 px-5"
+
+            ListHeaderComponent={() => (
+
+                <View className={"flex-between flex-row w-full my-5"}>
+                    <View className={"flex-start"}>
+                        <Text className={"small-bold text-primary"}>DELIVER TO</Text>
+                        <TouchableOpacity className={"flex-center flex-row gap-x-1 mt-0.5"}>
+                            <Text className={"paragraph-bold text-dark-100"}>Croatia</Text>
+                            <Image
+                                source={images.arrowDown}
+                                className={"size-3"}
+                                resizeMode={"contain"}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            )}
         />
       </SafeAreaView>
   );
