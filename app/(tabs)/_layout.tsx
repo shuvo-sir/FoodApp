fe5b5f7db192656d7533dc0fbdbfd072fefe09b3
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-28 16:33:08 +0600

    Create the appwrite setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Redirect, Slot} from "expo-router";
 
 export default function _Layout() {
-    const  isAuthenticated = false
+    const  isAuthenticated = true
 
     if(!isAuthenticated) return <Redirect href="/sign-in" />
     return (
