a933e4a8f0fdfd0e135b38f17f593578e210880e
Shuvo Halder <shuvohalder499@gmail.com>
2025-08-08 17:20:57 +0600

    create the sign in page and now focus on the sign up

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,38 @@
 import {View, Text, Button} from 'react-native'
 import React from 'react'
-import {router} from "expo-router";
+import {Link, router} from "expo-router";
+import CustomInput from "@/components/CustomInput";
+import CustomButtom from "@/components/CustomButtom";
 
 const SignIn = () => {
     return (
-        <View>
-            <Text>Sign In</Text>
-            <Button title="Sign In" onPress={() => router.push("/sign-up")} />
+        <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+
+            <CustomInput
+                placeholder={"Enter your email"}
+                value={""}
+                onChangeText={(text: string) => {}}
+                label={"Email"}
+                keyboardType={"email-address"}
+            />
+
+            <CustomInput
+                placeholder={"Enter your password"}
+                value={""}
+                onChangeText={(text: string) => {}}
+                label={"Password"}
+                secureTextEntry={true}
+            />
+
+            <CustomButtom
+                title={"Sign In"}
+            />
+
+            <View className={"base-regular text-gray-100"}>
+                <Text>Don't have an account?</Text>
+
+                <Link href={"/sign-up"} className={"base-bold text-primary"}>Sign Up</Link>
+            </View>
         </View>
     )
 }
